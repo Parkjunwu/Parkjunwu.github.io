b9cbf2b69232b1750358f7e11906aace86c5f9dc
Parkjunwu <phak9402@gmail.com>
2021-12-27 03:06:01 +0900

    Third commit

--- js/location.js
+++ js/location.js
@@ -5,7 +5,8 @@ if('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(async(position) => {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4b2f64ba5a0df00aee7fe6c6e5ba8a0f&units=metric`
+  console.log("lat : "+lat,"lon : "+lon)
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4b2f64ba5a0df00aee7fe6c6e5ba8a0f&units=metric`
   const weather = await fetch(url).then(res => res.json()).catch(()=>alert("Cannot get weather."))
   // console.log(weather)
   console.log(weather.name,
